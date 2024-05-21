c7a922f681c3449d567044f00aceaeed12238609
Riccardo Gulin <riccardo.gulin@gmail.com>
2024-05-21 12:41:42 +0200

    Adds loops examples

--- src/loops/Main.java
+++ src/loops/Main.java
@@ -1,10 +1,8 @@
 package loops;
 
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	/*	Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Dammi il primo numero");
 		int num1 = Integer.parseInt(scanner.nextLine()); // 20
@@ -29,7 +27,31 @@ public class Main {
 			System.out.println(num1 += 1); // ++num1
 		} while (num1 < 100);
 
-		scanner.close();
+
+		scanner.close();*/
+
+
+		System.out.println("------------------------ FOR ---------------------------");
+		/*for (int i = 0; i <= 10; i++) {
+			if (i % 2 == 0) continue; // continue serve per saltare l'iterazione corrente e passare alla successiva
+			System.out.println(i);
+		}
+
+		for (int i = 0; i <= 10; i++) {
+			if (i == 5) break; // break serve per uscire forzatamente dal loop
+			System.out.println(i);
+		}*/
+
+		esterno:
+		// Se io mettessi un'etichetta al loop esterno, poi con i break posso decidere di uscire da quel loop specifico
+		for (int i = 0; i < 5; i++) {
+			if (i == 3) continue;
+			for (int j = 0; j < 5; j++) {
+				if (j == 2) break esterno;
+				System.out.println("j: " + j);
+			}
+			System.out.println("i: " + i);
+		}
 	}
 
 }
